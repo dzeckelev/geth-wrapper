feef7184705b8c163b460eff4746da9553141751
bik <dzeckelev@gmail.com>
2019-03-20 15:27:58 +0700

    Decomposition test for handler GetLast method

--- api/handler_test.go
+++ api/handler_test.go
@@ -2,6 +2,7 @@ package api_test
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"fmt"
 	"math/big"
 	"os"
@@ -33,6 +34,8 @@ var (
 	dataBase  *reform.DB
 	sqlMock   sqlmock.Sqlmock
 	ethClient *eth.MockClient
+	columns   = []string{"o_id", "o_hash", "o_from", "o_to", "o_amount",
+		"o_status", "o_block", "o_timestamp", "o_marked", "o_confirmations"}
 )
 
 func newEthClient() *eth.MockClient {
@@ -56,9 +59,7 @@ func newEthClient() *eth.MockClient {
 	}
 }
 
-func TestGetLast(t *testing.T) {
-	limitArg := uint64(100)
-	confirmationsArg := uint64(3)
+func newTestTx() *data.Transaction {
 	hash := "0x64e604787cbf194841e7b68d7cd28786f6c9a0a3ab9f8b0a0e87cb4387ab0107"
 	from := "0xe7dc9fe68da458b54f648146a817126053eeef66"
 	to := "0xa7dba6053a0d631177340e8061bc12f5009ba453"
@@ -68,7 +69,7 @@ func TestGetLast(t *testing.T) {
 	timestamp := pointer.ToUint64(777777)
 	confirmations := uint64(1)
 
-	tx := &data.Transaction{
+	return &data.Transaction{
 		ID:            gen.NewUUID(),
 		Hash:          hash,
 		From:          from,
@@ -79,81 +80,91 @@ func TestGetLast(t *testing.T) {
 		Timestamp:     timestamp,
 		Confirmations: confirmations,
 	}
+}
 
-	tx2 := &data.Transaction{
-		ID:            gen.NewUUID(),
-		Hash:          hash,
-		From:          from,
-		To:            to,
-		Amount:        amount,
-		Status:        status,
-		Block:         block,
-		Timestamp:     timestamp,
-		Confirmations: confirmations,
+func createTestTxs(num int) (result []*data.Transaction) {
+	for i := 0; i < num; i++ {
+		result = append(result, newTestTx())
+	}
+	return result
+}
+
+func toRow(tx *data.Transaction) (result []driver.Value) {
+	result = make([]driver.Value, len(tx.Values()))
+
+	for k, v := range tx.Values() {
+		result[k] = v
 	}
 
-	txs := []*data.Transaction{tx, tx2}
+	return result
+}
 
-	columns := []string{"o_id", "o_hash", "o_from", "o_to", "o_amount",
-		"o_status", "o_block", "o_timestamp", "o_marked", "o_confirmations"}
+func checkFiled(t *testing.T, exp, got interface{}) {
+	if !reflect.DeepEqual(exp, got) {
+		t.Fatalf("expected %v, got %v", exp, got)
+	}
+}
 
-	// Empty result.
-	sqlMock.ExpectQuery(`SELECT (.+) FROM "transactions"`).
-		WithArgs(confirmationsArg, limitArg).WillReturnRows(sqlmock.NewRows(columns))
+func checkDate(t *testing.T, str string, timestamp *uint64) {
+	if timestamp != nil {
+		tm := time.Unix(int64(*timestamp), 0)
+		if !reflect.DeepEqual(tm.Format(time.RFC3339), str) {
+			t.Fatalf("expected %v, got %v", tm.Format(time.RFC3339), str)
+		}
+	}
+}
 
-	result, err := handler.GetLast(limitArg)
+func checkGetLast(t *testing.T,
+	limit uint64, expected int) []api.GetLastResult {
+	result, err := handler.GetLast(limit)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(result) != 0 {
-		t.Fatal("result must be empty")
+	if len(result) != expected {
+		t.Fatalf("expected %v, got %v", expected, len(result))
 	}
 
+	return result
+}
+
+func TestGetLast(t *testing.T) {
+	limit := uint64(100)
+	confirmations := uint64(3)
+
+	txs := createTestTxs(2)
+
+	expSelectSQL := `SELECT (.+) FROM "transactions"`
+	expUpdateSQL := `UPDATE "transactions"`
+
+	// Empty result.
+	sqlMock.ExpectQuery(expSelectSQL).
+		WithArgs(confirmations, limit).WillReturnRows(sqlmock.NewRows(columns))
+
+	checkGetLast(t, limit, 0)
+
 	// Normal test.
-	sqlMock.ExpectQuery(`SELECT (.+) FROM "transactions"`).
-		WithArgs(confirmationsArg, limitArg).WillReturnRows(
-		sqlmock.NewRows(columns).AddRow(
-			tx.ID, tx.Hash, tx.From, tx.To, tx.Amount, tx.Status,
-			tx.Block, tx.Timestamp, tx.Marked, tx.Confirmations).AddRow(
-			tx2.ID, tx2.Hash, tx2.From, tx2.To, tx2.Amount, tx2.Status,
-			tx2.Block, tx2.Timestamp, tx2.Marked, tx2.Confirmations))
+	sqlMock.ExpectQuery(expSelectSQL).
+		WithArgs(confirmations, limit).WillReturnRows(sqlmock.NewRows(columns).
+		AddRow(toRow(txs[0])...).AddRow(toRow(txs[1])...))
 
 	for range txs {
-		sqlMock.ExpectExec(`UPDATE "transactions"`).
+		sqlMock.ExpectExec(expUpdateSQL).
 			WillReturnResult(sqlmock.NewResult(1, 1))
 	}
 
-	result, err = handler.GetLast(limitArg)
-	if err != nil {
-		t.Fatal(err)
+	result := checkGetLast(t, limit, 2)
+
+	for k := range result {
+		checkFiled(t, result[k].Confirmations, txs[k].Confirmations)
+		checkFiled(t, result[k].Amount, txs[k].Amount)
+		checkFiled(t, result[k].Hash, txs[k].Hash)
+		checkFiled(t, result[k].Address, txs[k].To)
+		checkDate(t, result[k].Date, txs[k].Timestamp)
 	}
 
 	if err := sqlMock.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
-
-	checkFiled := func(exp, got interface{}) {
-		if !reflect.DeepEqual(exp, got) {
-			t.Fatalf("expected %v, got %v", exp, got)
-		}
-	}
-
-	checkDate := func(str string, timestamp *uint64) {
-		if timestamp != nil {
-			tm := time.Unix(int64(*timestamp), 0)
-			if !reflect.DeepEqual(tm.Format(time.RFC3339), str) {
-				t.Fatalf("expected %v, got %v", tm.Format(time.RFC3339), str)
-			}
-		}
-	}
-
-	for k := range result {
-		checkFiled(result[k].Confirmations, txs[k].Confirmations)
-		checkFiled(result[k].Amount, txs[k].Amount)
-		checkFiled(result[k].Hash, txs[k].Hash)
-		checkFiled(result[k].Address, txs[k].To)
-		checkDate(result[k].Date, txs[k].Timestamp)
 	}
 }
 
--- eth/mock.go
+++ eth/mock.go
@@ -12,6 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
+// MockClient is a mock for Ethereum client.
 type MockClient struct {
 	Acc     map[string]*bind.TransactOpts
 	NetID   *big.Int
@@ -19,11 +20,12 @@ type MockClient struct {
 	Backend *backends.SimulatedBackend
 }
 
+// NewMockClient creates a new Ethereum client.
 func NewMockClient() *MockClient {
 	return &MockClient{}
 }
 
-// Accounts gets accounts from Geth node.
+// Accounts is a mock for Accounts function.
 func (c *MockClient) Accounts(
 	ctx context.Context) (result []string, err error) {
 	for k := range c.Acc {
@@ -33,8 +35,7 @@ func (c *MockClient) Accounts(
 	return result, nil
 }
 
-// SendTransaction sends a transaction through Geth node.
-// Account must be unlocked.
+// SendTransaction is a mock for SendTransaction function.
 func (c *MockClient) SendTransaction(ctx context.Context,
 	from, to common.Address, amount *big.Int) (result *string, err error) {
 	gasLimit := uint64(4700000)
@@ -59,35 +60,42 @@ func (c *MockClient) SendTransaction(ctx context.Context,
 		return nil, err
 	}
 
+	c.Backend.Commit()
+
 	hash := strings.ToLower(signTx.Hash().String())
 	return &hash, nil
 }
 
+// NetworkID is a mock for NetworkID function.
 func (c *MockClient) NetworkID(ctx context.Context) (*big.Int, error) {
 	return c.NetID, nil
 }
 
+// BlockByNumber is a mock for BlockByNumber function.
 func (c *MockClient) BlockByNumber(ctx context.Context,
 	number *big.Int) (*types.Block, error) {
 	return c.Block, nil
 }
 
+// TransactionReceipt is a mock for TransactionReceipt function.
 func (c *MockClient) TransactionReceipt(ctx context.Context,
 	txHash common.Hash) (*types.Receipt, error) {
 	return c.TransactionReceipt(ctx, txHash)
 }
 
+// BalanceAt is a mock for BalanceAt function.
 func (c *MockClient) BalanceAt(ctx context.Context, account common.Address,
 	blockNumber *big.Int) (*big.Int, error) {
 	return c.BalanceAt(ctx, account, blockNumber)
 }
 
-// Block must be initialized.
+// SyncProgress is a mock for SyncProgress function.
 func (c *MockClient) SyncProgress(
 	ctx context.Context) (*ethereum.SyncProgress, error) {
 	return nil, nil
 }
 
+// NewTestBlock creates a new test block.
 func NewTestBlock(number *big.Int, txs []*types.Transaction,
 	trx []*types.Receipt) *types.Block {
 	header := &types.Header{
